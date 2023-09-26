d3b61d4f02a9273b6bc50ea8799cde612715706a
Enning94 <enningfestus@gmail.com>
2023-09-26 12:05:36 +0000

    Create store, action, and reducer to connect to API endpoint

---
+++ app/javascript/redux/greetingsReducer.js
@@ -0,0 +1,21 @@
+// Action
+const GET_MESSAGE = "greetings/message";
+
+
+// Action Creator
+const getMessage = (message) => ({ type: GET_MESSAGE, payload: message });
+
+
+// Reducer
+export default function greetingsReducer(state = [], action = {}) {
+    switch (action.type) {
+    
+      case GET_MESSAGE:
+        return action.payload.greeting;
+
+      default:
+        return state;
+    }
+  }
+
+export { getMessage };
